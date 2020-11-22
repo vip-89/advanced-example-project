7a2146c0ee5d5bc17a639159f856b1549b56b8ff
NNN <nicolaenmd@gmail.com>
2020-11-22 23:51:45 +0200

    add multiplication with 7

--- nicolae.js
+++ nicolae.js
@@ -1,4 +1,12 @@
-function Nicolae(a){
-    console.log(a)
+// function Nicolae(a){
+//     console.log(a)
+// }
+// export {Nicolae}
+
+function kolia() {
+    let i = 0;
+    while(i++<10){
+        console.log('7 * ${i} = ${7*i}');
+    }
 }
-export {Nicolae}
+kolia();
