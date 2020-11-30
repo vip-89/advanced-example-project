559d894bda14c510f11920d153351ef60e84962c
ZDmitrii11 <dima_zol@mail.com>
2020-11-30 19:34:13 +0200

    add js

---
+++ arrow-function/Dima-ArrowFunction/arrowF.js
@@ -0,0 +1,11 @@
+// Tema pentru acasa: de creat 20 functii de tip Declaration cu deferite situatii
+// De creat 25 functii de tip expression
+// De creat 40 functii de tip arrow
+//
+
+
+
+let arrSum = (x,y) =>{
+    return x+y
+}
+
